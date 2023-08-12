3a2b8f74e43b50db01bd24eb5b272db5ac596383
w4560000 <39783609+w4560000@users.noreply.github.com>
2023-08-12 15:01:27 +0800

    加入專案檔案。

---
+++ RedisClusterTest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RedisClusterTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
